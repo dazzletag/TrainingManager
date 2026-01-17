92c9a1a0f5cbd7647ad8cdc22991cc8e4122820e
Darren Taggart <Darren.taggart@bristolcarehomes.co.uk>
2026-01-17 11:28:23 +0000

    Exclude induction and flu vaccine courses from recommendations

--- backend/src/routes/recommendations.ts
+++ backend/src/routes/recommendations.ts
@@ -44,7 +44,9 @@ router.get("/next-courses", async (_req, res) => {
     INNER JOIN training_requirement tr ON tr.id = cs.courseId
     WHERE tr.enabled = 1
       AND tr.name NOT LIKE '%SCTV%'
-      AND tr.name NOT LIKE '%Competency%';
+      AND tr.name NOT LIKE '%Competency%'
+      AND tr.name NOT LIKE '%Induction%'
+      AND tr.name NOT LIKE '%Flu Vaccine%';
   `);
 
   const recommendations = rows
